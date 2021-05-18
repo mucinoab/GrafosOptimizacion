ff9bc1c586847e3417b4c4a19bc99a9433b0f752
Bruno A. Muciño <mucinoab@gmail.com>
2021-05-18 17:14:39 -0500

    Agrega estructura Hash Set

--- utils.go
+++ utils.go
@@ -2,6 +2,21 @@ package main
 
 import "math"
 
+// custom Hash Set
+type Set struct {
+	m map[string]struct{}
+}
+
+func set() *Set {
+	return &Set{make(map[string]struct{})}
+}
+
+func (s *Set) Add(k string) {
+	s.m[k] = struct{}{}
+}
+
+// various utilities
+
 func Find(slice *[]camino, val camino) bool {
 	for _, item := range *slice {
 		if item == val {
@@ -20,9 +35,28 @@ func Min(marcado *[]camino, grafo map[string]map[string]float64) float64 {
 	mini := math.Inf(0)
 	for _, v := range *marcado {
 		e := grafo[v.izq][v.der]
-		if e < mini {
-			mini = e
-		}
+		mini = math.Min(mini, e)
 	}
 	return mini
+}
+
+func VerticesToAdjList(grafo *[]Vertice, dirigido bool) map[string]map[string]float64 {
+	adjList := make(map[string]map[string]float64)
+
+	for _, a := range *grafo {
+		if _, ok := adjList[a.Origen]; !ok {
+			adjList[a.Origen] = make(map[string]float64)
+		}
+
+		adjList[a.Origen][a.Destino] = a.Peso
+
+		if !dirigido {
+			if _, ok := adjList[a.Destino]; !ok {
+				adjList[a.Destino] = make(map[string]float64)
+			}
+			adjList[a.Destino][a.Origen] = a.Peso
+		}
+	}
+
+	return adjList
 }
