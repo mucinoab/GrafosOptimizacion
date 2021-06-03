38ed4dddf7c719a1e0e8f1508c91addb9216e8bb
Bruno A. Muciño <mucinoab@gmail.com>
2021-06-03 16:59:30 -0500

    Añade ejemplo a Dijkstra.

--- js/examples.ts
+++ js/examples.ts
@@ -16,9 +16,9 @@ function ejemploFloyd() {
   const ejemplo = [{"origen":"1","destino":"2","peso":700}, {"origen":"1","destino":"3","peso":200},{"origen":"2","destino":"3","peso":300},{"origen":"2","destino":"4","peso":200},{"origen":"2","destino":"6","peso":400},{"origen":"3","destino":"4","peso":700},{"origen":"3","destino":"5","peso":600},{"origen":"4","destino":"6","peso":100},{"origen":"4","destino":"5","peso":300},{"origen":"6","destino":"5","peso":500}];
   fillTableEjemplo("Floyd", ejemplo);
   postData('floydwarshall', ejemplo).then(data => {renderResponseFloyd(data);});
-  }
+}
 
-  function ejemploCPM() {
+function ejemploCPM() {
   const ejemplo = [{"origen":"A","destino":"-","peso":2},{"origen":"B","destino":"A","peso":4},{"origen":"C","destino":"B","peso":1},{"origen":"C","destino":"H","peso":1},{"origen":"D","destino":"-","peso":6},{"origen":"E","destino":"G","peso":3},{"origen":"F","destino":"E","peso":5},{"origen":"G","destino":"D","peso":2},{"origen":"H","destino":"G","peso":2},{"origen":"I","destino":"D","peso":3},{"origen":"J","destino":"I","peso":4},{"origen":"K","destino":"D","peso":3},{"origen":"L","destino":"J","peso":5},{"origen":"L","destino":"K","peso":5},{"origen":"M","destino":"C","peso":2},{"origen":"M","destino":"L","peso":2}];
   fillTableEjemplo("CPM", ejemplo);
   postData('cpm', ejemplo).then(data => {renderResponseCPM(data);});
@@ -28,10 +28,18 @@ function ejemploPERT() {
   const ejemplo = [{"origen":"A","destino":"-","optimista":1,"probable":2,"pesimista":3},{"origen":"B","destino":"A","optimista":2,"probable":4,"pesimista":6},{"origen":"C","destino":"B","optimista":0,"probable":1,"pesimista":2},{"origen":"C","destino":"H","optimista":0,"probable":1,"pesimista":2},{"origen":"D","destino":"-","optimista":3,"probable":6,"pesimista":9},{"origen":"E","destino":"G","optimista":2,"probable":3,"pesimista":4},{"origen":"F","destino":"E","optimista":3,"probable":5,"pesimista":7},{"origen":"G","destino":"D","optimista":1,"probable":2,"pesimista":3},{"origen":"H","destino":"G","optimista":1,"probable":2,"pesimista":3},{"origen":"I","destino":"D","optimista":1,"probable":3,"pesimista":5},{"origen":"J","destino":"I","optimista":3,"probable":4,"pesimista":5},{"origen":"K","destino":"D","optimista":2,"probable":3,"pesimista":4},{"origen":"L","destino":"J","optimista":3,"probable":5,"pesimista":7},{"origen":"L","destino":"K","optimista":3,"probable":5,"pesimista":7},{"origen":"M","destino":"C","optimista":1,"probable":2,"pesimista":3},{"origen":"M","destino":"L","optimista":1,"probable":2,"pesimista":3}];
   fillTableEjemplo("PERT", ejemplo);
   postData('pert', ejemplo).then(data => {renderResponsePERT(data);});
-  }
+}
 
-  function ejemploCompresion() {
+function ejemploCompresion() {
   const ejemplo = {"actividades":[{"actividad":"A","predecesora":"-","pesoNormal":8,"costoNormal":100,"pesoUrgente":6,"costoUrgente":200},{"actividad":"B","predecesora":"-","pesoNormal":4,"costoNormal":150,"pesoUrgente":2,"costoUrgente":350},{"actividad":"C","predecesora":"A","pesoNormal":2,"costoNormal":50,"pesoUrgente":1,"costoUrgente":90},{"actividad":"D","predecesora":"B","pesoNormal":5,"costoNormal":100,"pesoUrgente":1,"costoUrgente":200},{"actividad":"E","predecesora":"C","pesoNormal":3,"costoNormal":80,"pesoUrgente":1,"costoUrgente":100},{"actividad":"E","predecesora":"D","pesoNormal":3,"costoNormal":80,"pesoUrgente":1,"costoUrgente":100},{"actividad":"F","predecesora":"A","pesoNormal":10,"costoNormal":100,"pesoUrgente":5,"costoUrgente":400}],"tiempoObjetivo":-10};
   fillTableEjemplo("Compresion", ejemplo.actividades);
   postData('compresion', ejemplo).then(data => {renderResponseCompresion(data);});
+}
+
+function ejemploDijkstra() {
+  const ejemplo = {"origen":"1","destino":"3","grafo":[{"origen":"1","destino":"2","peso":3},{"origen":"2","destino":"3","peso":5},{"origen":"1","destino":"5","peso":1},{"origen":"5","destino":"7","peso":1},{"origen":"7","destino":"8","peso":1},{"origen":"8","destino":"3","peso":1}]};
+  (<HTMLInputElement>document.getElementById("OrigenDijkstra")).value = ejemplo.origen;
+  (<HTMLInputElement>document.getElementById("DestinoDijkstra")).value = ejemplo.destino;
+  fillTableEjemplo("Dijkstra", ejemplo.grafo);
+  postData('dijkstra', ejemplo).then(data => renderResponseDijkstra(data));
 }
